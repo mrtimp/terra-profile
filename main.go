f675d681a8989f94fb5a3844b731759dbdec02e4
Tim Philips <timp@unfuddle.nz>
2025-05-21 10:00:39 +1200

    Tidy up and add TG_NON_INTERACTIVE support

--- main.go
+++ main.go
@@ -22,14 +22,101 @@ type AccountConfig struct {
 }
 
 type Options struct {
-	Debug       bool   `short:"d" long:"debug" description:"Enable debug output"`
-	AccountFile string `short:"a" long:"account" description:"Account file" default:"account.hcl"`
-	Cmd         struct {
+	Debug          bool   `short:"d" long:"debug" description:"Enable debug output"`
+	NonInteractive bool   `short:"n" long:"non-interactive" description:"Disable Terragrunt interactive mode"`
+	AccountFile    string `short:"a" long:"account" description:"Account file" default:"account.hcl"`
+	Cmd            struct {
 		Args []string `positional-arg-name:"CMD" required:"yes"`
 	} `positional-args:"yes" required:"yes"`
 }
 
 var opts Options
+
+func main() {
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if opts.Debug {
+		log.SetLevel(log.DebugLevel)
+		log.SetFormatter(&log.TextFormatter{
+			ForceColors:   true,
+			FullTimestamp: false,
+		})
+	}
+
+	if len(opts.Cmd.Args) == 0 {
+		log.Errorf("Usage: terra-profile <command> [args...]")
+		os.Exit(1)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("Could not get current directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	hclPath, err := findAccountHCLFile(cwd)
+	if err != nil {
+		log.Errorf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	accountName, err := getAccountNameFromHCLFile(hclPath)
+	if err != nil {
+		log.Errorf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if opts.Debug {
+		log.Debugf("Found %s file in: %s\n", opts.AccountFile, filepath.Dir(hclPath))
+		log.Debugf("HCL file: account_name=%s\n", accountName)
+	}
+
+	err = os.Setenv("AWS_PROFILE", accountName)
+	if err != nil {
+		log.Errorf("Error setting environment variable: %v\n", err)
+		return
+	}
+
+	if opts.Debug {
+		log.Debugf("Setting envirnoment variable: AWS_PROFILE=%s\n", accountName)
+	}
+
+	if opts.NonInteractive {
+		err = os.Setenv("TG_NON_INTERACTIVE", "true")
+		if err != nil {
+			log.Errorf("Error setting environment variable: %v\n", err)
+			return
+		}
+
+		if opts.Debug {
+			log.Debug("Setting envirnoment variable: TG_NON_INTERACTIVE=true\n")
+		}
+	}
+
+	cmd := exec.Command(opts.Cmd.Args[0], opts.Cmd.Args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Env = os.Environ()
+
+	if opts.Debug {
+		log.Debugf("Executing: %v\n", cmd.Args)
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			os.Exit(exitErr.ExitCode())
+		}
+
+		log.Errorf("Error running command: %v\n", err)
+		os.Exit(1)
+	}
+}
 
 func findGitRoot(startDir string) (string, error) {
 	dir := startDir
@@ -94,78 +181,4 @@ func getAccountNameFromHCLFile(hclPath string) (string, error) {
 	accountName := config.Locals.AccountName
 
 	return accountName, nil
-}
-
-func main() {
-	_, err := flags.Parse(&opts)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	if opts.Debug {
-		log.SetLevel(log.DebugLevel)
-		log.SetFormatter(&log.TextFormatter{
-			ForceColors:   true,
-			FullTimestamp: false,
-		})
-	}
-
-	if len(opts.Cmd.Args) == 0 {
-		log.Errorf("Usage: terra-profile <command> [args...]")
-		os.Exit(1)
-	}
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Errorf("Could not get current directory: %v\n", err)
-		os.Exit(1)
-	}
-
-	hclPath, err := findAccountHCLFile(cwd)
-	if err != nil {
-		log.Errorf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	accountName, err := getAccountNameFromHCLFile(hclPath)
-	if err != nil {
-		log.Errorf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	if opts.Debug {
-		log.Debugf("Found %s file in: %s\n", opts.AccountFile, filepath.Dir(hclPath))
-		log.Debugf("HCL file: account_name=%s\n", accountName)
-	}
-
-	err = os.Setenv("AWS_PROFILE", accountName)
-	if err != nil {
-		log.Errorf("Error setting environment variable: %v\n", err)
-		return
-	}
-
-	if opts.Debug {
-		log.Debugf("Setting envirnoment variable: AWS_PROFILE=%s\n", accountName)
-	}
-
-	cmd := exec.Command(opts.Cmd.Args[0], opts.Cmd.Args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	cmd.Env = os.Environ()
-
-	if opts.Debug {
-		log.Debugf("Executing: %v\n", cmd.Args)
-	}
-
-	err = cmd.Run()
-	if err != nil {
-		var exitErr *exec.ExitError
-		if errors.As(err, &exitErr) {
-			os.Exit(exitErr.ExitCode())
-		}
-
-		log.Errorf("Error running command: %v\n", err)
-		os.Exit(1)
-	}
 }
